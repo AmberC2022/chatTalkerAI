3b3e32cb2f83a3dbc28dd81964df0aef90b0bd24
AmberC2022 <105841938+AmberC2022@users.noreply.github.com>
2022-06-02 22:26:19 +0800

    增加 FAQ toggle 效果

--- js/all.js
+++ js/all.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
+    $('.faq-ans').hide();
+    $('.minus').hide();
+    $('.faq-title').click(function(){
+        $(this).find('.minus').toggle();
+        $(this).find('.plus').toggle();
 
-   
-
-
-});
+        $(this).toggleClass('active');
+        $(this).siblings('.faq-ans').slideToggle();
+        // siblings().removeClass('active');
+        // siblings().slideup();
+    })
+    $('btn-top').click(function(e){
+        e.preventDefault();
+        $('html').scrollTop(0);
+    })
+    
+    
+})
 

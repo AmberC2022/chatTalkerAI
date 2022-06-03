1b60d4ee5a5a1285714eab0b3709531ff33a5a3f
AmberC2022 <105841938+AmberC2022@users.noreply.github.com>
2022-06-03 10:10:48 +0800

    增加 text() 語法

--- js/all.js
+++ js/all.js
@@ -1,20 +1,19 @@
 $(document).ready(function(){
     $('.faq-ans').hide();
     $('.minus').hide();
+    $('.number-of-people').click(function(e){
+        e.preventDefault();
+        $('.font-num').text('500')
+    })
     $('.faq-title').click(function(){
         $(this).find('.minus').toggle();
         $(this).find('.plus').toggle();
-
         $(this).toggleClass('active');
         $(this).siblings('.faq-ans').slideToggle();
-        // siblings().removeClass('active');
-        // siblings().slideup();
     })
     $('btn-top').click(function(e){
         e.preventDefault();
         $('html').scrollTop(0);
     })
-    
-    
 })
 
